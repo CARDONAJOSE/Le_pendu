aa94a83c5f378b58ed3292b788535751677e2404
youssef <yossef.majdane.pro@gmail.com>
2024-09-16 15:06:45 +0200

    error counter implemented

--- Pendu.py
+++ Pendu.py
@@ -1,18 +1,20 @@
 ## Le pendu
 
 import random
+import matplotlib.pyplot as plt
 
 def randomaccesword():
     wordlist = ["banana", "strawberry", "kiwi", "Orange", "Apple"]
     wordchose = random.choice(wordlist)
     return wordchose
 word = randomaccesword()
-maskedword = ["" for _ in word]
+maskedword = ["_" for _ in word]
 tentatives = 5
 lettersguessed = []
 print("Mot masqué :"+ "_" * len(maskedword))
 #print("Mot masqué :", "".join(maskedword))
 
+error = 0
 while tentatives > 0 and "_" in maskedword:
     guess = input("Devinez une lettre : ").lower()
     if len(guess) != 1 or not guess.isalpha():
@@ -24,7 +26,6 @@ while tentatives > 0 and "_" in maskedword:
         continue
 
     lettersguessed.append(guess)
-
     if guess in word:
         for index, letter in enumerate(word):
             if letter == guess:
@@ -32,11 +33,13 @@ while tentatives > 0 and "_" in maskedword:
         print("Bonne réponse !")
     else:
         tentatives -= 1
-        print(f"Lettre incorrecte. Il vous reste {tentatives} tentatives.")
+        error += 1
+        print("error est egale a ", error)
+    print(f"Lettre incorrecte. Il vous reste {tentatives} tentatives.")
 
     print("Mot masqué :", " ".join(maskedword))
 if "" not in maskedword:
     print("Félicitations ! Vous avez gagné !")
 else:
     print(f"Désolé, vous avez perdu. Le mot était '{word}'.")
-    
+
