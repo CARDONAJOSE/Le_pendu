cab2ff8234e2f700e9050660ccbdd64df47aa05b
youssef <yossef.majdane.pro@gmail.com>
2024-09-16 11:00:16 +0200

    first

--- Pendu.py
+++ Pendu.py
@@ -1,1 +1,19 @@
 ## Le pendu
+
+import random
+
+def randomaccesword():
+    wordlist = ["banana", "strawberry", "kiwi"]
+    wordchose = random.choice(wordlist)
+    return wordchose
+    
+word = randomaccesword()
+print(word)
+
+def checkletters():
+    array = randomacceswordword()
+    letterinputted = input()
+    for i in range array
+        if letterinputted == i:
+        return letterinputted
+    
