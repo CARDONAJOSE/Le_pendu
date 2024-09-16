ae8b5cffd8b7ff1b890445fb5003084fa0bbf8a1
youssef <yossef.majdane.pro@gmail.com>
2024-09-16 14:07:39 +0200

    dsdsd

--- Pendu.py
+++ Pendu.py
@@ -3,17 +3,42 @@
 import random
 
 def randomaccesword():
-    wordlist = ["banana", "strawberry", "kiwi"]
+    wordlist = ["banana", "strawberry", "kiwi", "Orange", "Apple"]
     wordchose = random.choice(wordlist)
     return wordchose
-    
-word = randomaccesword()
-print(word)
 
-def checkletters():
-    array = randomacceswordword()
-    letterinputted = input()
-    for i in array:
-        if letterinputted == i:
-        return letterinputted
-    
+word = randomaccesword()
+maskedword = ["_" for _ in word]
+tentatives = 5
+lettersguessed = []
+
+print("Mot masqué :", " ".join(maskedword))
+
+while tentatives > 0 and "" in maskedword:
+    guess = input("Devinez une lettre : ").lower()
+
+    if len(guess) != 1 or not guess.isalpha():
+        print("Veuillez entrer une seule lettre.")
+        continue
+
+    if guess in lettersguessed:
+        print("Vous avez déjà deviné cette lettre.")
+        continue
+
+    letters_guessed.append(guess)
+
+    if guess in word:
+        for index, letter in enumerate(word):
+            if letter == guess:
+                maskedword[index] = guess
+        print("Bonne réponse !")
+    else:
+        tentatives -= 1
+        print(f"Lettre incorrecte. Il vous reste {tentatives} tentatives.")
+
+    print("Mot masqué :", " ".join(maskedword))
+
+if "" not in maskedword:
+    print("Félicitations ! Vous avez gagné !")
+else:
+    print(f"Désolé, vous avez perdu. Le mot était '{word}'.")
